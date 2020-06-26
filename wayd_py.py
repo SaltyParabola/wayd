a639dc1c0a00af6130ed548e28e5a01c5961adce
SaltyParabola <65374368+SaltyParabola@users.noreply.github.com>
2020-06-25 22:51:13 -0400

    Add files via upload
    Version of wayd that uses only python and nothing specific to macs. The file can be run directly from the command line without any additional files. Use $ pip install schedule

---
+++ wayd_py.py
@@ -0,0 +1,58 @@
+import PySimpleGUI as sg
+from datetime import datetime
+from os.path import isfile
+import schedule
+import time
+
+#package wayd to be a function
+def wayd():
+	#folder where dailies are stored
+	folder_path = r'C:\Users\Kalea\Documents\wayd\wayd'
+	
+	#layout of input box
+	wayd_layout = [
+	    [sg.Text('What are you doing?')],
+	    [sg.InputText()],
+	    [sg.Submit(), sg.Cancel()]
+	]
+	
+	#create input box, take input
+	window = sg.Window('wayd', wayd_layout)
+	event, values = window.read()
+	
+	
+	#time that input was entered
+	time = datetime.now().strftime('%H:%M')
+	
+	#today's date in two formats
+	date_dash = datetime.now().strftime('%Y-%m-%d')
+	date_dot = datetime.now().strftime('%d.%m.%Y')
+	
+	#close input box
+	window.close()
+	
+	#full path to daily note
+	full_path = folder_path + date_dash + '.md'
+	
+	#if content has been entered
+	if values[0] != '':
+	    #if file already exists
+	    if isfile(full_path):
+	        f = open(folder_path + date_dash + '.md','a+')
+	        f.write('\n* ' + time + ': ' + values[0])
+	        f.close()
+	    #if file doesn't exist (aka program hasn't run today)
+	    else:
+	        f = open(folder_path + date_dash + '.md','w+')
+	        f.write('# ' + date_dot + '\n## today i will\n>==thrive==>\n\n## ideas // thoughts // things i did\n* ' + time + ': ' + values[0])
+	        f.close()
+	#if no content entered
+	else:
+	    exit()
+
+#add wayd function to queue every 15 minutes
+schedule.every(15).minutes.do(wayd)
+
+#execute everything to queue as added
+while True:
+	schedule.run_pending()
